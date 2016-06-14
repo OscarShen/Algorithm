698cb3653fe05fa2b6b56a40439d0fc051a45671
Oscar <oscarshen09@foxmail.com>
2016-06-14 22:15:30 +0800

    slove MaxSubseqSum with "divide and conquer" method.

--- src/maxSubseqSum/test.java
+++ src/maxSubseqSum/test.java
@@ -3,6 +3,6 @@ package maxSubseqSum;
 public class test {
 	public static void main(String[] args) {
 		int[] a = { -2, 11, -4, 13, -5, -2 };
-		System.out.println(new MaxSubseqSum01().maxSum2(a, a.length));
+		System.out.println(new MaxSubseqSum02().maxSum3(a, a.length));
 	}
 }
