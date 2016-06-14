9f63a4bf878260027870847715a6bb5a78d1aaa1
Oscar <oscarshen09@foxmail.com>
2016-06-14 21:28:29 +0800

    two exhaustive attack methods achieve max subsequence sum

---
+++ src/maxSubseqSum/test.java
@@ -0,0 +1,8 @@
+package maxSubseqSum;
+
+public class test {
+	public static void main(String[] args) {
+		int[] a = { -2, 11, -4, 13, -5, -2 };
+		System.out.println(new MaxSubseqSum01().maxSum2(a, a.length));
+	}
+}
