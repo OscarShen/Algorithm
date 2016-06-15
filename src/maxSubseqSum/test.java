e0470942d90ff368fe59c647fc2e19f36f3daad7
ruiyao.shen <oscarshen09@foxmail.com>
2016-06-15 17:57:37 +0800

    not only solve max subsequence sum but find where the numbers are.

--- src/maxSubseqSum/test.java
+++
@@ -1,8 +0,0 @@
-package maxSubseqSum;
-
-public class test {
-	public static void main(String[] args) {
-		int[] a = { -2, 11, -4, 13, -5, -2 };
-		System.out.println(new MaxSubseqSum02().maxSum3(a, a.length));
-	}
-}
