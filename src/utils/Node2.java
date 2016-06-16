dc01f78ac2dd347a3e17423915061bf79a615cf0
ruiyao.shen <oscarshen09@foxmail.com>
2016-06-16 09:49:25 +0800

    I will finish after my work!!!!!

--- src/utils/Node2.java
+++ src/utils/Node2.java
@@ -1,8 +1,8 @@
 package utils;
 
 public class Node2 {
-	public int a;
-	public int b;
+	private int a;
+	private int b;
 	public static final int INFINITY = Integer.MAX_VALUE;
 	public Node2 next;
 
@@ -12,10 +12,30 @@ public class Node2 {
 		this.next = null;
 	}
 
-	public Node2(int a, int b, Node2 next) {
+	public Node2(int a, int b) {
 		this.a = a;
 		this.b = b;
-		this.next = next;
+		this.next = null;
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public static int getInfinity() {
+		return INFINITY;
 	}
 
 }
